951009e6c2a42853947130749fd77e8a797a09f3
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-09-19 16:03:34 +0300

    update: added notes hook with react swr

---
+++ hooks/useNotes.js
@@ -0,0 +1,14 @@
+import useSWR from 'swr';
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+export default function useNotes() {
+  const { data, error, mutate } = useSWR('/api/note', fetcher);
+
+  return {
+    notes: data,
+    mutate,
+    isError: !data && error,
+    isLoading: !data && !error,
+  };
+}
