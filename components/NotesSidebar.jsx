61dc010203e2167bfdd7086613f84714d50f0667
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-09-19 21:39:13 +0300

    update: added basic sidebar markup for notes

--- components/NotesSidebar.jsx
+++ components/NotesSidebar.jsx
@@ -7,22 +7,45 @@ export default function NotesSidebar({
   onSelectNote,
   onDeleteNote,
 }) {
-  const getSmallText = useCallback((text) => `${text.slice(0, 15)}...`, []);
+  const formatDate = useCallback(
+    (date) =>
+      Intl.DateTimeFormat('ua', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+      }).format(new Date(date)),
+    [],
+  );
 
   return (
-    <ul className={styles.list}>
-      {notes.map((note) => (
-        <li
-          key={note.id}
-          onClick={onSelectNote}
-          className={`${styles.list__item} ${
-            currentNote.id === note.id ? styles['list__item--selected'] : ''
-          }`}
-        >
-          <h2>{note.title}</h2>
-          <small>{getSmallText(note.content)}</small>
-        </li>
-      ))}
-    </ul>
+    <div className={styles.main}>
+      <div className={styles.main__wrapper}>
+        <input
+          type="text"
+          placeholder="Search with note title"
+          className={styles.main__wrapper__input}
+        />
+      </div>
+      <ul className={styles.main__list}>
+        {notes.map((note) => (
+          <li
+            key={note.id}
+            onClick={() => onSelectNote(note)}
+            className={`${styles.main__list__item} ${
+              currentNote && currentNote.id === note.id
+                ? styles['main__list__item--selected']
+                : ''
+            }`}
+          >
+            <h2 className={styles.main__list__item__heading}>
+              {note.title || 'New Note'}
+            </h2>
+            <small className={styles.main__list__item__createdAt}>
+              {formatDate(note.createdAt)}
+            </small>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
