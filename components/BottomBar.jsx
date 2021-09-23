259db2d364e984238e56bb0cfdb40f05e929762a
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-09-23 23:50:16 +0300

    update: added some little tooltips for bottom bar items

--- components/BottomBar.jsx
+++ components/BottomBar.jsx
@@ -31,6 +31,7 @@ export default function BottomBar({
         <li
           className={`${styles.main__list__item} ${styles['main__list__item--svg']}`}
           onClick={onToggleMarkdownPreview}
+          title="Toggle markdown preview"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -43,6 +44,7 @@ export default function BottomBar({
         <li
           className={`${styles.main__list__item} ${styles['main__list__item--svg']} ${styles['main__list__item--delete']}`}
           onClick={onDeleteNote}
+          title="Delete current note"
         >
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
             <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zm2.46-7.12l1.41-1.41L12 12.59l2.12-2.12 1.41 1.41L13.41 14l2.12 2.12-1.41 1.41L12 15.41l-2.12 2.12-1.41-1.41L10.59 14l-2.13-2.12zM15.5 4l-1-1h-5l-1 1H5v2h14V4z" />
