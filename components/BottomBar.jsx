0ece7fbf28c697c4c9fedd4b4de1e9cc57f14750
Bogdan Kostyuk (laptop)) <bogdankostyuk12@gmail.com>
2021-09-28 18:24:14 +0300

    update: added button with svg in bottom bar for adding note in favorites

--- components/BottomBar.jsx
+++ components/BottomBar.jsx
@@ -49,6 +49,15 @@ export default function BottomBar({
           </svg>
         </li>
         <li
+          className={`${styles.main__list__item} ${styles['main__list__item--svg']} ${styles['main__list__item--star']}`}
+          // onClick={onSelectScratchPad}
+          title="Add to favorites"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M22 9.24l-7.19-.62L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27 18.18 21l-1.63-7.03L22 9.24zM12 15.4l-3.76 2.27 1-4.28-3.32-2.88 4.38-.38L12 6.1l1.71 4.04 4.38.38-3.32 2.88 1 4.28L12 15.4z" />
+          </svg>
+        </li>
+        <li
           className={`${styles.main__list__item} ${styles['main__list__item--svg']} ${styles['main__list__item--delete']}`}
           onClick={onDeleteNote}
           title="Delete current note"
